90d368910da74bb8c0dddecefa50ee44935bd449
Dylan Graham <dylan.graham@gmail.com>
2017-03-10 11:25:39 +1100

    tidy

--- hello.js
+++ hello.js
@@ -1,24 +1,21 @@
+const http = require('http');
+const fs = require('fs');
+
 http.createServer((req, res) => {
     getTitles(res);
 }).listen(8000, '127.0.0.1');
 
 function getTitles(res) {
     fs.readFile('./titles.json', (err, data) => {
-        if (err) {
-            hadError(err, res);
-        } else {
-            getTemplate(JSON.parse(data.toString()), res);
-        }
+        if (err) hadError(err, res);
+        else getTemplate(JSON.parse(data.toString()), res);
     });
 }
 
 function getTemplate(titles, res) {
     fs.readFile('./template.html', (err, data) => {
-        if (err) {
-            hadError(err, res);
-        } else {
-            formatHtml(titles, data.toString(), res);
-        }
+        if (err) hadError(err, res);
+        else formatHtml(titles, data.toString(), res);
     });
 }
 
