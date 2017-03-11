492ad78252586f62b57501e336c8a5df2725f49a
Dylan Graham <dylan.graham@gmail.com>
2017-03-11 14:46:09 +1100

    body parser

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const articles = [{title: 'Example'}];
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/articles', (req, res, next) => {
     res.send(articles);
 });
 
 app.post('/articles', (req, res, next) => {
-    res.send('OK');
+    const article = {title: req.body.title};
+    articles.push(article);
+    res.send(article);
 });
 
 app.get('/articles/:id', (req, res, next) => {
