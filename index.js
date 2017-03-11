ff6c376b679522696ad7a5047f0892862142d389
Dylan Graham <dylan.graham@gmail.com>
2017-03-11 14:18:57 +1100

    basic express app

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+const articles = [{title: 'Example'}];
+
+app.get('/articles', (req, res, next) => {
+    res.send(articles);
+});
+
+app.post('/articles', (req, res, next) => {
+    res.send('OK');
+});
+
+app.get('/articles/:id', (req, res, next) => {
+    const id = req.params.id;
+    console.log('Fetching: ', id);
+    res.send(articles[id]);
+});
+
+app.delete('/articles/:id', (req, res, next) => {
+    const id = req.params.id;
+    console.log('Deleting: ', id);
+    delete articles[id];
+    res.send({message: 'Deleted'});
+});
+
+app.listen(process.env.PORT || 3000);
+
+module.exports = app;
