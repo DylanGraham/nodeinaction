fbb224f885cd30a5ca2ad592d73b4028dd692cba
Dylan Graham <dylan.graham@gmail.com>
2017-03-11 15:05:05 +1100

    add model

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
-
-const articles = [{title: 'Example'}];
+const app = express();
+const Article = require('./db').Article;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/articles', (req, res, next) => {
-    res.send(articles);
+    Article.all((err, articles) => {
+        if (err) return next(err);
+        res.send(articles);
+    });
 });
 
 app.post('/articles', (req, res, next) => {
@@ -19,15 +21,18 @@ app.post('/articles', (req, res, next) => {
 
 app.get('/articles/:id', (req, res, next) => {
     const id = req.params.id;
-    console.log('Fetching: ', id);
-    res.send(articles[id]);
+    Article.find(id, (err, article) => {
+        if (err) return next(err);
+        res.send(article);
+    });
 });
 
 app.delete('/articles/:id', (req, res, next) => {
     const id = req.params.id;
-    console.log('Deleting: ', id);
-    delete articles[id];
-    res.send({message: 'Deleted'});
+    Article.delete(id, err => {
+        if (err) return next(err);
+        res.send({message: 'Deleted'});
+    });
 });
 
 app.listen(process.env.PORT || 3000);
